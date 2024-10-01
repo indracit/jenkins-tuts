90da6f0537d64b8a7f9f7d46097ef65673f01eee
Indracit S <indracits@integramicro.co.in>
2024-10-01 17:36:32 +0530

    added handling route

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express();
 
-app.use((req,res)=>{
+app.use('/',(req,res)=>{
 res.send({"message":"hi"})
 })
+
+app.use('/*',(req,res)=>{
+    res.send({"message":"not found"})
+    })
 
 app.listen(3001,()=>{
     console.log('app url: http://localhost:3001');
