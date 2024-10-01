54aea3af1a3859aaad20678005da7207107c2262
Indracit S <indracits@integramicro.co.in>
2024-10-01 17:45:28 +0530

    added home route

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 app.use('/',(req,res)=>{
 res.send({"message":"hi"})
 })
+app.use('/home',(req,res)=>{
+    res.send({"message":"welcome home"})
+    })
 
 app.use('/*',(req,res)=>{
     res.send({"message":"not found"})
